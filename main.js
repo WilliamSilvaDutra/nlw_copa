a4f6344e6905376e0f4e86b79f84023fe8937f5a
William da Silva Dutra <98925964+WilliamSilvaDutra@users.noreply.github.com>
2022-11-06 10:51:08 -0300

    commit

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function createGame(player1, hour, player2){
     </li>
  `
 }
-
+let appear = 1
 function createCard(date, day, games) {
+appear = appear + 1.5
  return `
-   <div class="card">
+   <div class="card" style = "animation: appear ${appear}s">
        <h2>${date} <span>${day}</span></h2>
        <ul>
           ${games}
@@ -19,23 +20,25 @@ function createCard(date, day, games) {
     `
 }
 
-document.getElementById("app").innerHTML = `
-        <header>
-            <img src="./assets/logo.svg" alt="Logo NLW">
-        </header>
-        <main id="cards">
-            ${createCard(
-             "20/11",
-             "domingo",
-             createGame("qatar", "13:00", "ecuador")
-            )}
-            ${createCard(
-             "21/11",
-             "segunda",
-             createGame("england", "10:00", "iran") +
-             createGame("senegal", "13:00", "netherlands") +
-             createGame("usa", "16:00", "wales")
-            )}
-            
-        </main>
-        `
+document.getElementById("cards").innerHTML =
+        
+    createCard(
+        "20/11",
+        "domingo",
+        createGame("qatar", "13:00", "ecuador")
+    )+
+    createCard(
+        "21/11",
+        "segunda",
+        createGame("england", "10:00", "iran") +
+        createGame("senegal", "13:00", "netherlands") +
+        createGame("usa", "16:00", "wales")
+
+    ) +
+     createCard(
+      "21/11",
+      "segunda",
+      createGame("england", "10:00", "iran") +
+       createGame("senegal", "13:00", "netherlands") +
+       createGame("usa", "16:00", "wales")
+     )
